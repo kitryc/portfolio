91d62951720574fcec1d3ef0b152dd506fb80425
Virak <106400@GLTHS.GLTECH.ORG>
2025-01-13 12:41:36 -0500

    Fix

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ buttons.forEach(button => {
     });
 });
 
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('a');
 
@@ -54,7 +54,37 @@ window.onload = () => {
             }, 500);
         });
     }
-};
+});
+
+// window.onload = () => {
+//     const transition_el = document.querySelector('.transition');
+//     const anchors = document.querySelectorAll('a');
+
+//     setTimeout(() => {
+//         transition_el.classList.remove('is-active');
+//     }, 500);
+
+//     for (let i = 0; i < anchors.length; i++) {
+//         const anchor = anchors[i];
+
+//         if (anchor.target === "_blank") {
+//             continue;
+//         }
+
+//         anchor.addEventListener('click', e => {
+//             const target = anchor.href;
+
+//             e.preventDefault();
+
+            
+//             transition_el.classList.add('is-active');
+
+//             setTimeout(() => {
+//                 window.location.href = target;
+//             }, 500);
+//         });
+//     }
+// };
 
 // window.addEventListener('popstate', () => {
 //     const transition_el = document.querySelector('.transition');
