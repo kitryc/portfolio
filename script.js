188c84c819a95e9034d915db8ca52874a8e293d7
Virak <106400@GLTHS.GLTECH.ORG>
2025-01-13 13:05:21 -0500

    Fix

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ buttons.forEach(button => {
     });
 });
 
+function navigate(target) {
+    const transition_el = document.querySelector('.transition');
+    transition_el.classList.add('is-active');
+
+    setTimeout(() => {
+        window.location.href = target;
+    }, 500);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('a');
@@ -22,24 +31,6 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
         transition_el.classList.remove('is-active');
     }, 500);
-
-    for (let i = 0; i < anchors.length; i++) {
-        const anchor = anchors[i];
-
-        if (anchor.target === "_blank") {
-            continue;
-        }
-
-        anchor.addEventListener('click', e => {
-            const target = anchor.href;
-            
-            //transition_el.classList.add('is-active');
-
-            setTimeout(() => {
-                window.location.href = target;
-            }, 500);
-        });
-    }
 });
 
 window.onbeforeunload = () => {
