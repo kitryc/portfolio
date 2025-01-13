3acebad41736ee6673e0b491d8835b27be7038c3
Virak <106400@GLTHS.GLTECH.ORG>
2025-01-13 12:55:50 -0500

    Fix

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// const glow = document.getElementById('glow');
-
-// document.addEventListener('mousemove', (event) => {
-//     const { clientX, clientY } = event;
-//     glow.style.left = `${clientX}px`;
-//     glow.style.top = `${clientY}px`;
-// });
-
-// grays out the other buttons when one is selected
-//don't ask how this works pls
 const buttons = document.querySelectorAll('.nav');
-
 buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
         buttons.forEach(b => {
@@ -43,11 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         anchor.addEventListener('click', e => {
             const target = anchor.href;
-
-            //e.preventDefault();
-
             
-            transition_el.classList.add('is-active');
+            //transition_el.classList.add('is-active');
 
             setTimeout(() => {
                 window.location.href = target;
@@ -55,44 +41,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
-// window.onload = () => {
-//     const transition_el = document.querySelector('.transition');
-//     const anchors = document.querySelectorAll('a');
-
-//     setTimeout(() => {
-//         transition_el.classList.remove('is-active');
-//     }, 500);
-
-//     for (let i = 0; i < anchors.length; i++) {
-//         const anchor = anchors[i];
-
-//         if (anchor.target === "_blank") {
-//             continue;
-//         }
-
-//         anchor.addEventListener('click', e => {
-//             const target = anchor.href;
-
-//             e.preventDefault();
-
-            
-//             transition_el.classList.add('is-active');
-
-//             setTimeout(() => {
-//                 window.location.href = target;
-//             }, 500);
-//         });
-//     }
-// };
-
-// window.addEventListener('popstate', () => {
-//     const transition_el = document.querySelector('.transition');
-//     if (transition_el) {
-//         transition_el.classList.add('is-active');
-//     }
-// });
-
 
 window.onbeforeunload = () => {
     const transition_el = document.querySelector('.transition');
